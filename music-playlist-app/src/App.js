11c4c41f70f21debdaabb34b9bb831378cbdb2bc
Mofeid Moshreky <mof_ton@yahoo.com>
2023-07-23 22:43:17 -0400

    update appjs

--- music-playlist-app/src/App.js
+++ music-playlist-app/src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import PlaylistList from "./components/PlaylistList";
+import AddPlaylistForm from "./components/AddPlaylistForm";
+import Navbar from "./components/Navbar";
+import "./App.css";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<PlaylistList />} />
+        <Route path="/my-playlists" element={<AddPlaylistForm />} />
+        {/* Add more routes for About and other components */}
+      </Routes>
     </div>
   );
-}
+};
 
 export default App;
